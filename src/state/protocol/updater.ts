c1163cd25eb52d8d838a0135ca72c8c08bea5ca1
danny7even <danny7even@nugglabs.org>
2022-01-03 03:35:22 +0100

    subscriptions setup

--- src/state/protocol/updater.ts
+++ src/state/protocol/updater.ts
@@ -1,4 +1,5 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
+import { gql, useSubscription } from '@apollo/client';
 
 import useRecursiveTimeout from '../../hooks/useRecursiveTimeout';
 import {
@@ -6,6 +7,8 @@ import {
     isUndefinedOrNullOrObjectEmpty,
 } from '../../lib';
 import constants from '../../lib/constants';
+import { _metaBare } from '../../graphql/fragments/_meta';
+import { client,  } from '../../graphql/client';
 
 import ProtocolState from '.';
 
@@ -23,9 +26,29 @@ export default () => {
         }
     }, [epoch, block]);
 
-    useRecursiveTimeout(() => {
-        checkEpoch();
-        ProtocolState.dispatch.updateBlock();
-    }, constants.QUERYTIME);
+    const { data } = useSubscription(
+        gql`
+            subscription Cool {
+                _meta {
+                    block {
+                        number
+                    }
+                }
+            }
+        `,
+        { client },
+    );
+
+    useEffect(() => {
+        if (data && data._meta && data._meta.block && data._meta.block.number) {
+            ProtocolState.dispatch.setCurrentBlock(data._meta.block.number);
+            checkEpoch();
+        }
+    }, [data]);
+
+    // useRecursiveTimeout(() => {
+    //     checkEpoch();
+    //     ProtocolState.dispatch.updateBlock();
+    // }, constants.QUERYTIME);
     return null;
 };
--- src/state/swap/updater.ts
+++ src/state/swap/updater.ts
@@ -1,16 +1,41 @@
+import { gql, useSubscription } from '@apollo/client';
+
 import useRecursiveTimeout from '../../hooks/useRecursiveTimeout';
 import { isUndefinedOrNullOrStringEmpty } from '../../lib';
 import constants from '../../lib/constants';
+import { client } from '../../graphql/client';
 
 import SwapState from '.';
 
 export default () => {
     const swapId = SwapState.select.id();
-    useRecursiveTimeout(() => {
-        if (!isUndefinedOrNullOrStringEmpty(swapId)) {
-            SwapState.dispatch.pollOffers({ swapId });
-        }
-    }, constants.QUERYTIME);
+
+    const { data } = useSubscription(
+        gql`
+            subscription Cool($swapId: ID!) {
+                swap(id: $swapId) {
+                    offers(orderBy: eth, orderDirection: desc) {
+                        id
+                        user {
+                            id
+                        }
+                        eth
+                        ethUsd
+                        claimed
+                        owner
+                    }
+                }
+            }
+        `,
+        { client, variables: { swapId } },
+    );
+
+    console.log(data);
+    // useRecursiveTimeout(() => {
+    //     if (!isUndefinedOrNullOrStringEmpty(swapId)) {
+    //         SwapState.dispatch.pollOffers({ swapId });
+    //     }
+    // }, constants.QUERYTIME);
 
     return null;
 };
