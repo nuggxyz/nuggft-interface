ccaa1981eb33fa37f83bd5672a871d4efa8d4bc0
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-02-20 12:04:54 -0600

    filtering massive errors in webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -375,7 +375,22 @@ module.exports = function (webpackEnv) {
                     enforce: 'pre',
                     exclude: /@babel(?:\/|\\{1,2})runtime/,
                     test: /\.(js|mjs|jsx|ts|tsx|css)$/,
-                    loader: require.resolve('source-map-loader'),
+                    use: [
+                        {
+                            loader: 'source-map-loader',
+                            options: {
+                                filterSourceMappingUrl: (url, resourcePath) => {
+                                    if (
+                                        resourcePath.includes('@walletconnect')
+                                    ) {
+                                        return 'skip';
+                                    }
+
+                                    return true;
+                                },
+                            },
+                        },
+                    ],
                 },
                 {
                     // "oneOf" will traverse all following loaders until one will
