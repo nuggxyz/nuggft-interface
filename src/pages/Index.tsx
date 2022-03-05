7b70346c6926efd019da02121b49552186cf9575
danny7even <90333397+danny7even@users.noreply.github.com>
2022-03-05 15:03:44 +0100

    WHOAMI

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Confetti from 'react-confetti';
 
 import Desktop from '@src/structure/desktop';
 import Mobile from '@src/structure/mobile';
@@ -7,8 +6,8 @@ import state from '@src/state';
 
 const IndexPage = () => {
     const type = state.app.select.screenType();
-    const isOver = state.protocol.select.epochIsOver();
-    const { width, height } = state.app.select.dimensions();
+    // const isOver = state.protocol.select.epochIsOver();
+    // const { width, height } = state.app.select.dimensions();
     return (
         <>
             {/* {isOver && <Confetti {...{ width, height }} recycle={false} numberOfPieces={500} />} */}
