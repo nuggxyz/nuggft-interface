cd25b4c451b9be700ca86ecbc388df432e2da410
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-04-18 21:53:17 -0500

    lets see how this feels

--- src/pages/mobile/MobileSwapPage.tsx
+++ src/pages/mobile/MobileSwapPage.tsx
@@ -9,13 +9,26 @@ const SwapView = () => {
     const recentNuggs = client.live.recentSwaps();
     const activeItems = client.live.activeItems();
     const recentItems = client.live.recentItems();
+
+    const potentialSwaps = client.live.potentialSwaps();
+    const potentialItems = client.live.potentialItems();
+
+    console.log(potentialItems, potentialSwaps);
+
     const epoch = client.live.epoch.id();
 
     const all = React.useMemo(() => {
-        return [...activeNuggs, ...activeItems, ...recentItems, ...recentNuggs].sort((a, b) =>
-            a.eth.lt(b.eth) ? -1 : 1,
+        return [
+            ...activeNuggs,
+            ...activeItems,
+            ...recentItems,
+            ...recentNuggs,
+            ...potentialSwaps,
+            ...potentialItems,
+        ].sort((a, b) =>
+            a.isCurrent === b.isCurrent ? (a.eth.lt(b.eth) ? -1 : 1) : a.isCurrent ? 1 : -1,
         );
-    }, [activeNuggs, activeItems, recentItems, recentNuggs]);
+    }, [activeNuggs, activeItems, recentItems, recentNuggs, potentialItems, potentialSwaps]);
 
     const sortedAll = React.useMemo(() => {
         return all.reduce(
@@ -24,21 +37,24 @@ const SwapView = () => {
                     current: SwapData[];
                     next: SwapData[];
                     recent: SwapData[];
+                    potential: SwapData[];
                 },
                 curr,
             ) => {
-                if (epoch && curr.endingEpoch) {
+                if (epoch) {
                     if (curr.endingEpoch === epoch) {
                         prev.current.push(curr);
                     } else if (curr.endingEpoch === epoch + 1) {
                         prev.next.push(curr);
-                    } else if (curr.endingEpoch < epoch) {
+                    } else if (curr.endingEpoch === 0) {
+                        prev.potential.push(curr);
+                    } else if (curr.endingEpoch && curr.endingEpoch < epoch) {
                         prev.recent.push(curr);
                     }
                 }
                 return prev;
             },
-            { current: [], next: [], recent: [] },
+            { current: [], next: [], recent: [], potential: [] },
         );
     }, [all, epoch]);
 
@@ -64,6 +80,10 @@ const SwapView = () => {
 
             {sortedAll.next.map((x) => (
                 <SwapCard tokenId={x.tokenId} key={`SwapCard-Next-${x.tokenId}`} />
+            ))}
+
+            {sortedAll.potential.map((x) => (
+                <SwapCard tokenId={x.tokenId} key={`SwapCard-Potential-${x.tokenId}`} />
             ))}
         </div>
     );
