71d1a6ebdf87d2520e61e86a27248924a4d2bc37
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-05-14 08:28:10 -0500

    copying types from zustand

--- src/client/play.ts
+++ src/client/play.ts
@@ -1,6 +1,21 @@
 /* eslint-disable no-param-reassign */
-import create from 'zustand';
-import { combine } from 'zustand/middleware';
+import create, { GetState, SetState, StoreApi } from 'zustand';
+import { NamedSet } from 'zustand/middleware/devtools';
+
+declare type Combine<T, U> = Omit<T, keyof U> & U;
+
+export declare const combine: <PrimaryState extends object, SecondaryState extends object>(
+    initialState: PrimaryState,
+    create: (
+        set: SetState<PrimaryState> & NamedSet<PrimaryState>,
+        get: GetState<PrimaryState>,
+        api: StoreApi<PrimaryState>,
+    ) => SecondaryState,
+) => (
+    set: SetState<Combine<PrimaryState, SecondaryState>>,
+    get: GetState<Combine<PrimaryState, SecondaryState>>,
+    api: StoreApi<Combine<PrimaryState, SecondaryState>>,
+) => Combine<PrimaryState, SecondaryState>;
 
 const stateA = combine(
     {
