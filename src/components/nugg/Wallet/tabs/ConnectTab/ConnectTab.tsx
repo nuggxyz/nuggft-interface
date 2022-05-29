e25bb58c1fbe324263a0741c807f48a40197d96c
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-05-29 17:54:39 -0500

    fixing some styles in the mobile connect

--- src/components/nugg/Wallet/tabs/ConnectTab/ConnectTab.tsx
+++ src/components/nugg/Wallet/tabs/ConnectTab/ConnectTab.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import curriedLighten from 'polished/lib/color/lighten';
 import { t } from '@lingui/macro';
+import { IoChevronDownCircle } from 'react-icons/io5';
 
 import Button from '@src/components/general/Buttons/Button/Button';
 import NLStaticImage from '@src/components/general/NLStaticImage';
@@ -8,6 +9,8 @@ import Text from '@src/components/general/Texts/Text/Text';
 import web3 from '@src/web3';
 import useDimensions from '@src/client/hooks/useDimensions';
 import lib from '@src/lib';
+import client from '@src/client';
+import { Page } from '@src/interfaces/nuggbook';
 
 import styles from './ConnectTab.styles';
 
@@ -15,9 +18,9 @@ type Props = Record<string, never>;
 
 const ConnectTab: FunctionComponent<Props> = () => {
     const { isPhone } = useDimensions();
-
+    const goto = client.nuggbook.useOpenNuggBook();
     const [forceDesktopAction, setForceDesktopAction] = React.useState(false);
-    const [showAll] = React.useState(true);
+    const [showAll, setShowAll] = React.useState(!isPhone);
     return (
         <div
             style={{
@@ -33,6 +36,8 @@ const ConnectTab: FunctionComponent<Props> = () => {
             <div
                 style={{
                     ...styles.disclaimerContainer,
+                    padding: '1rem',
+
                     background: isPhone
                         ? styles.walletsContainer.background
                         : styles.disclaimerContainer.background,
@@ -43,6 +48,39 @@ const ConnectTab: FunctionComponent<Props> = () => {
                     that you have read and understood the nuggft Protocol Disclaimer.`}
                 </Text>
             </div>
+
+            {isPhone && (
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'flex-start',
+                        alignItems: 'center',
+                        marginTop: -15,
+                        marginBottom: 10,
+                        borderRadius: lib.layout.borderRadius.mediumish,
+                        backgroundColor: lib.colors.transparentWhite,
+                        padding: 15,
+                        WebkitBackdropFilter: 'blur(50px)',
+                        backdropFilter: 'blur(50px)',
+                    }}
+                >
+                    <Text textStyle={{ marginBottom: 5 }} size="small">
+                        lost?
+                    </Text>
+                    <Button
+                        textStyle={{ color: 'white', fontWeight: lib.layout.fontWeight.thicc }}
+                        buttonStyle={{
+                            background: lib.colors.primaryColor,
+                            borderRadius: lib.layout.borderRadius.large,
+                            boxShadow: lib.layout.boxShadow.dark,
+                        }}
+                        label="we gotchu 😉 ➡️"
+                        onClick={() => goto(Page.Setup_0, true)}
+                    />
+                </div>
+            )}
+
             <div
                 style={{
                     background: lib.colors.transparentWhite,
@@ -80,63 +118,63 @@ const ConnectTab: FunctionComponent<Props> = () => {
                             <NLStaticImage image={`${Peer.CoinbaseWallet}_icon`} />
                             <NLStaticImage image={`${Peer.MetaMask}_icon`} />
                             <NLStaticImage image={`${Peer.Rainbow}_icon`} />
-                        </div>
-
-                        <Button
-                            className="mobile-pressable-div"
-                            buttonStyle={{
-                                margin: 10,
-                                backgroundColor: lib.colors.transparentWhite,
-                                color: lib.colors.primaryColor,
-                                borderRadius: lib.layout.borderRadius.mediumish,
-                                WebkitBackdropFilter: 'blur(50px)',
-                                backdropFilter: 'blur(50px)',
-                                alignItems: 'center',
-                            }}
-                            textStyle={{ ...lib.layout.presets.font.main.thicc }}
-                            label="show all"
-                            leftIcon={
-                                <IoChevronDownCircle
-                                    color={lib.colors.primaryColor}
-                                    style={{ marginRight: '.3rem' }}
-                                    size={20}
-                                />
-                            }
-                            onClick={() => {
-                                setShowAll(true);
-                            }}
-                        /> */}
+                        </div> */}
                     </div>
                 )}
-                {(!isPhone || showAll) &&
-                    Object.values(web3.config.peers).map(
-                        (peer) =>
-                            !peer.fallback && (
-                                <Button
-                                    className="mobile-pressable-div"
-                                    key={peer.name}
-                                    buttonStyle={{
-                                        ...styles.walletButton,
-                                        border: `5px solid ${curriedLighten(0.1)(peer.color)}`,
-                                    }}
-                                    rightIcon={<NLStaticImage image={peer.peer} />}
-                                    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-                                    onClick={async () => {
-                                        const check = web3.config.connector_instances[peer.type];
+                {Object.values(web3.config.peers).map((peer, ind) =>
+                    !peer.fallback && (!isPhone || showAll || ind < 2) ? (
+                        <Button
+                            className="mobile-pressable-div"
+                            key={peer.name}
+                            buttonStyle={{
+                                ...styles.walletButton,
+                                border: `5px solid ${curriedLighten(0.1)(peer.color)}`,
+                            }}
+                            rightIcon={<NLStaticImage image={peer.peer} />}
+                            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                            onClick={async () => {
+                                const check = web3.config.connector_instances[peer.type];
 
-                                        if (check) {
-                                            if (check.store.getState().activating)
-                                                await check.connector.deactivate();
-                                            void check.connector.activate(
-                                                undefined,
-                                                peer.peer,
-                                                __DEV__ && forceDesktopAction,
-                                            );
-                                        }
-                                    }}
-                                />
-                            ),
-                    )}
+                                if (check) {
+                                    if (check.store.getState().activating)
+                                        await check.connector.deactivate();
+                                    void check.connector.activate(
+                                        undefined,
+                                        peer.peer,
+                                        __DEV__ && forceDesktopAction,
+                                    );
+                                }
+                            }}
+                        />
+                    ) : null,
+                )}
+
+                {!showAll && (
+                    <Button
+                        className="mobile-pressable-div"
+                        buttonStyle={{
+                            margin: 10,
+                            backgroundColor: lib.colors.transparentWhite,
+                            color: lib.colors.primaryColor,
+                            borderRadius: lib.layout.borderRadius.mediumish,
+                            WebkitBackdropFilter: 'blur(50px)',
+                            backdropFilter: 'blur(50px)',
+                            alignItems: 'center',
+                        }}
+                        textStyle={{ ...lib.layout.presets.font.main.thicc }}
+                        label="show all"
+                        leftIcon={
+                            <IoChevronDownCircle
+                                color={lib.colors.primaryColor}
+                                style={{ marginRight: '.3rem' }}
+                                size={20}
+                            />
+                        }
+                        onClick={() => {
+                            setShowAll(true);
+                        }}
+                    />
+                )}
 
                 {isPhone && __DEV__ && (
                     <Button
