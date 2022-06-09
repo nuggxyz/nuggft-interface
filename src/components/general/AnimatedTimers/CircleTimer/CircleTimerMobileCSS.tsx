89f2b34309274a5ed271dacf81962accfac4912f
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-06-09 18:32:22 -0500

    bye bye springy ring

--- src/components/general/AnimatedTimers/CircleTimer/CircleTimerMobileCSS.tsx
+++ src/components/general/AnimatedTimers/CircleTimer/CircleTimerMobileCSS.tsx
@@ -2,7 +2,6 @@ import React, { CSSProperties, FunctionComponent, ReactChild, useMemo } from 're
 
 import lib from '@src/lib';
 import useInterval from '@src/hooks/useInterval';
-import packages from '@src/packages';
 
 import styles from './CircleTimer.styles';
 
@@ -18,7 +17,7 @@ type Props = {
     strokeWidth?: number;
     defaultColor: string;
     tokenId?: TokenId;
-    spring?: boolean;
+    // spring?: boolean;
 };
 const TWOPI = Math.PI * 2;
 const HALFPI = Math.PI / 2;
@@ -35,7 +34,7 @@ const CircleTimerMobileCSS: FunctionComponent<Props> = ({
     defaultColor,
     staticColor: _staticColor,
     tokenId,
-    spring = true,
+    // spring = false,
 }) => {
     const staticColor = React.useMemo(() => {
         return _staticColor ?? duration === remaining ? 'white' : null;
@@ -101,24 +100,26 @@ const CircleTimerMobileCSS: FunctionComponent<Props> = ({
             width / 6.5 + 50,
             `${(width / 6.5) * TWOPI} ${(width / 6.5) * TWOPI}`,
             shadowColor === 'transparent' ? 'transparent' : 'white',
-            spring ? {} : { transition: `all 2s ${lib.layout.animation}` },
+            // spring ? {} : { transition: `all 2s ${lib.layout.animation}` },
+            { transition: `all 2s ${lib.layout.animation}` },
+
             activated ? strokeWidth : 0,
             `drop-shadow(2px 3px 10px ${shadowColor}) hue-rotate(0)`,
         ];
-    }, [width, shadowColor, activated, strokeWidth, spring]);
+    }, [width, shadowColor, activated, strokeWidth]);
 
-    const todef = React.useDeferredValue(to);
+    // const todef = React.useDeferredValue(to);
 
-    const [{ x }] = packages.spring.useSpring(
-        {
-            to: {
-                x: todef,
-            },
-            immediate: !spring,
-            config: packages.spring.config.molasses,
-        },
-        [todef, spring],
-    );
+    // const [{ x }] = packages.spring.useSpring(
+    //     {
+    //         to: {
+    //             x: todef,
+    //         },
+    //         immediate: !spring,
+    //         config: packages.spring.config.molasses,
+    //     },
+    //     [todef, spring],
+    // );
 
     return (
         <div style={{ zIndex: 1, ...style }}>
@@ -156,12 +157,12 @@ const CircleTimerMobileCSS: FunctionComponent<Props> = ({
                         transform: 'rotate(-90deg)',
                     }}
                 >
-                    <packages.spring.animated.circle
+                    <circle
                         cx="50%"
                         cy="50%"
                         r={r}
                         stroke={_shadowColor}
-                        strokeDashoffset={x}
+                        strokeDashoffset={to}
                         strokeWidth={_strokeWidth}
                         fill="none"
                         strokeDasharray={strokeDashArray}
